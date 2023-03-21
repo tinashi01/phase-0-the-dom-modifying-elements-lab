5927009371bffb49922e22fc69882c7bd166490d
Tina Shi <init.defaultBranch>
2023-03-21 22:56:04 +1100

    commit tree 87e62989e5225ff33409a8a0a9153d9acc0cb1fc

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('#main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Tina is the champion"
+
